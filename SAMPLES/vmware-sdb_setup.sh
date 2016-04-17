6fee60e185ed7b6d698872fd9372cc8c45b65b56
Nitin Madhok <nmadhok@g.clemson.edu>
2016-04-17 11:22:35 -0400

    Also configure cloud provider configuration for VMware cloud driver and test connection

--- SAMPLES/vmware-sdb_setup.sh
+++ SAMPLES/vmware-sdb_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
 # This script sets up SDB in salt master configuration and stores credentials used by the VMware cloud
-# driver. Salt-master, salt-cloud and salt-minion must be installed and configured on the system to be
-# able to use this script.
+# driver in a sqlite database. It also configures the cloud provider configuration for the VMware cloud
+# driver. Salt-master, salt-cloud and salt-minion must be installed on the system to be able to use this
+# script.
 #
 # Author Name:		Nitin Madhok
 # Author Email:		nmadhok@clemson.edu
@@ -21,17 +22,19 @@ PASSWORD='passwordForUser'
 URL='vcenter01.domain.com'
 ESXI_HOST_USER='root'
 ESXI_HOST_PASSWORD='passwordForESXiHostUser'
-DB_FILE_PATH='/etc/salt/cloud.providers.d/vmware.sqlite'
+SDB_PATH='/etc/salt/cloud.providers.d/vmware.sqlite'
+VMWARE_CLOUD_CONFIG_FILE='/etc/salt/cloud.providers.d/vmware.conf'
+VMWARE_SDB_CONFIG_FILE='/etc/salt/master.d/vmware_sdb.conf'
 
 
 ####################################################################################################
 ######################################## Main Script Begins ########################################
 ####################################################################################################
 
-cat >/etc/salt/master.d/vmware_sdb.conf <<EOL
+cat >$VMWARE_SDB_CONFIG_FILE <<EOL
 ${ID}:
   driver: sqlite3
-  database: ${DB_FILE_PATH}
+  database: ${SDB_PATH}
   table: ${ID}
   create_table: True
 EOL
@@ -43,5 +46,15 @@ salt-run sdb.set "sdb://$ID/url" "$URL" &
 salt-run sdb.set "sdb://$ID/esxi_host_user" "$ESXI_HOST_USER" &
 salt-run sdb.set "sdb://$ID/esxi_host_password" "$ESXI_HOST_PASSWORD" &
 wait
-chmod 600 $DB_FILE_PATH
+chmod 600 $SDB_PATH
+cat >$VMWARE_CLOUD_CONFIG_FILE <<EOL
+${ID}:
+  driver: vmware
+  user: 'sdb://$ID/user'
+  password: 'sdb://$ID/password'
+  url: 'sdb://$ID/url'
+  esxi_host_user: 'sdb://$ID/esxi_host_user'
+  esxi_host_password: 'sdb://$ID/esxi_host_password'
+EOL
+salt-cloud -f test_vcenter_connection $ID
 exit 0
